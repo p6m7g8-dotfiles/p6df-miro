2d9d4f310d0dfa06fa878eacf2956f86ca596fa6
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-04-01 11:03:41 -0400

    fix(prompt): split p6_return_words word and variable into separate args
    Refactor p6df-miro to use p6_return_words with separate word/variable args,
    add README.md, update deps to include p6common, and replace prompt::mod/profile::on/off
    with a simplified profile::mod.

--- init.zsh
+++ init.zsh
@@ -7,70 +7,21 @@
 #>
 ######################################################################
 p6df::modules::miro::deps() {
-  ModuleDeps=()
+  ModuleDeps=(p6m7g8-dotfiles/p6common)
 }
 
 ######################################################################
 #<
 #
-# Function: str str = p6df::modules::miro::prompt::mod()
+# Function: words miro $MIRO_API_KEY = p6df::modules::miro::profile::mod()
 #
 #  Returns:
-#	str - str
+#	words - miro $MIRO_API_KEY
 #
-#  Environment:	 P6_DFZ_PROFILE_MIRO
+#  Environment:	 MIRO_API_KEY
 #>
 ######################################################################
-p6df::modules::miro::prompt::mod() {
-  local str=""
-  local profile="$P6_DFZ_PROFILE_MIRO"
+p6df::modules::miro::profile::mod() {
 
-  if p6_string_blank_NOT "$profile"; then
-    str="miro:\t\t  ${profile}"
-  fi
-
-  p6_return_str "$str"
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::miro::profile::on(profile, code)
-#
-#  Args:
-#	profile -
-#	code - shell code block
-#
-#  Environment:	 P6_DFZ_PROFILE_MIRO
-#>
-######################################################################
-p6df::modules::miro::profile::on() {
-  local profile="$1"
-  local code="$2"
-
-  p6_run_code "$code"
-
-  p6_env_export "P6_DFZ_PROFILE_MIRO" "$profile"
-
-  p6_return_void
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::miro::profile::off(code)
-#
-#  Args:
-#	code - shell code block previously passed to profile::on
-#
-#  Environment:	 P6_DFZ_PROFILE_MIRO
-#>
-######################################################################
-p6df::modules::miro::profile::off() {
-  local code="$1"
-
-  p6_env_unset_from_code "$code"
-  p6_env_export_un P6_DFZ_PROFILE_MIRO
-
-  p6_return_void
+  p6_return_words 'miro' '$MIRO_API_KEY'
 }
